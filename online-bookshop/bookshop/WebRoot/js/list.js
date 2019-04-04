bf5ba461701f6bc1c6bec3ac293a79cbc7181e92
xloya <982052490@qq.com>
2019-04-04 11:44:29 +0800

    更新

--- online-bookshop/bookshop/WebRoot/js/list.js
+++
@@ -1,7 +0,0 @@
-$(function() {
-	$(".inner").hover(function() {
-		$(this).css("border", "3px solid #FFCFB1");
-	}, function() {
-		$(this).css("border", "3px solid #ffffff");
-	});
-});
--- online-bookshop/bookshop/src/com/lxhf/constants/Constants.java
+++
@@ -1,8 +0,0 @@
-package com.lxhf.constants;
-
-public interface Constants {
-	
-	Integer PAGESIZE = 5;
-	Integer CATEGORYPAGESIZE = 3;
-	String CAR = "car";
-}
