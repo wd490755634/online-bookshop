6762218a7f053916cd2f34ad18fc4e026f000308
xloya <982052490@qq.com>
2019-04-13 11:18:30 +0800

    delete erroe

--- online-bookshop/bookshop/WebRoot/js/desc.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-	$("#cnt").blur(function() {
-		var quantity = $("#cnt").val();
-		if(!/^[1-9]\d*$/.test(quantity)) {
-			alert("数量必须是合法整数！");
-			$("#cnt").val("1");
-		}
-	});
-});
